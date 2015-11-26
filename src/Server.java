684f2c3016bb06fef6f988a33b4bde4ae27ae10b
cbyu <yu.chad@hotmail.com>
2015-11-26 12:13:18 -0500

    Testing for loop

--- src/Server.java
+++ src/Server.java
@@ -3,6 +3,10 @@ public class Server{
     public static void main(String[] args)
     {
         System.out.println("Hello WOrld");
+        for(int i = 0; i < 10; i++ )
+        {
+            System.out.println(i + "\n");
+        }
     }
 
 
